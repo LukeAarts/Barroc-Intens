85ae9977a3cdf5f8129230facbb76d0fa4e977ff
Aykut0234 <93645887+Aykut0234@users.noreply.github.com>
2023-11-22 17:45:39 +0100

    note model aangemaakt

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Maintenance::class);
     }
+    
+        public function authoredNotes()
+    {
+        return $this->hasMany(Note::class, 'author_id');
+    }
 }
