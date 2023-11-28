0d1f4d7a90229f0a1533f898950fe9250b89659b
Aykut0234 <93645887+Aykut0234@users.noreply.github.com>
2023-11-28 09:28:47 +0100

    user notes blade toegevoegd, dat is een pagina als je op een gebruiker klikt dat je zijn notities kan zien of verwijderen

---
+++ resources/views/notes/user_notes.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Notities van {{ $user->name }}</h1>
+    
+    @if ($notes)
+        <ul>
+            @foreach($notes as $note)
+                <li>
+                    <p>{{ $note->note }}</p>
+                    <p>Aangemaakt op: {{ $note->date }}</p>
+                    <a href="{{ route('notes.destroy', ['note' => $note]) }}" onclick="event.preventDefault(); document.getElementById('delete-form-{{ $note->id }}').submit();">Verwijder</a>
+
+                    <!-- Verwijderingsformulier verbergen -->
+                    <form id="delete-form-{{ $note->id }}" action="{{ route('notes.destroy', ['note' => $note]) }}" method="post" style="display: none;">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                </li>
+            @endforeach
+        </ul>
+        <a href="{{ route('notes.create', ['user_id' => $user->id]) }}">Nieuwe Notitie Toevoegen</a>
+        <a href="{{ route('notes.index') }}">Terug naar Gebruikers</a>
+    @else
+        <p>Geen notities beschikbaar voor {{ $user->name }}</p>
+        <a href="{{ route('notes.create', ['user_id' => $user->id]) }}">Nieuwe Notitie Toevoegen</a>
+        <a href="{{ route('notes.index') }}">Terug naar Gebruikers</a>
+    @endif
+@endsection
