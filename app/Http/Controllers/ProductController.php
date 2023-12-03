15c1f378a91ebc21986634a8f5adb72513cd669b
Thomas Vermeer <THQgameworld@gmail.com>
2023-12-03 14:25:26 +0100

    create product save image in public storage en database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,10 +38,23 @@ class ProductController extends Controller
 
         ]);
 
-        Product::create($request->post());
+
 
         $filename = $request->file('image_path')->getClientOriginalName();
         $request->file('image_path')->storeAs('public', $filename);
+
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->stock = $request->input('stock');
+        $product->product_category_id = $request->input('product_category_id');
+        $product->image_path = $filename;
+        $product->save();
+
+
+
+
 
         return Redirect::route('purchases_products.index')->with('success', 'Product is succesvol toegevoegd.');
     }
