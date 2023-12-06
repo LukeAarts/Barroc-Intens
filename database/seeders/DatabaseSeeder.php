d50bc05f6d6e8cdc803fc4d1482a8c765b59b86e
Thomas Vermeer <THQgameworld@gmail.com>
2023-12-06 13:51:08 +0100

    leasecontractSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\LeaseContract;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +17,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
+
             ProductCategorySeeder::class,
+            Lease_contractSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
---
+++ database/seeders/Lease_contractSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\LeaseContract;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Lease_contractSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        LeaseContract::create([
+
+            'name' => 'order1',
+            'description' => 'in dit contract gaat het over het leasen van koffiemachine 1',
+            'type' => 'maandelijks',
+            'is_signed' => false,
+            'customer_id' => 5,
+
+
+
+
+        ]);
+    }
+}
