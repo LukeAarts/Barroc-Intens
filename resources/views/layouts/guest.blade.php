870695980094afab05951d36e9b046961505e7e4
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2023-12-13 15:12:30 +0100

    update index pagina products. styling hetzelfde als de homepagina.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,33 +7,38 @@
     @vite('resources/css/app.css')
 </head>
 <body class="font-sans">
+    <div class="relative min-h-screen">
+        <div class="absolute inset-0 bg-center bg-cover" style="background-image: url('images/-1200-1200.jpg');"></div>
+        <div class="relative z-10 bg-black bg-opacity-75 text-white">
+                <!-- Navigatiebalk -->
+                <nav class="p-4 text-white">
+                    <div class="container mx-auto flex items-center w-full">
+                        <img src="images/logo6_groot.png" style="width: 65px" alt="afbeelding">
+                        <ul class="flex ml-24 space-x-4 text-xl font-light">
+                            <li><a href="{{route('home')}}" class="hover:text-gray-300">Home</a></li>
+                            <li><a href="{{route('products.index')}}" class="hover:text-gray-300">Producten</a></li>
+                            <li><a href="#contact" class="hover:text-gray-300">Contact</a></li>
+                        </ul>
+                        @if(\Illuminate\Support\Facades\Auth::user() != null && !\Illuminate\Support\Facades\Auth::user()->hasRole('Customer'))
+                            <ul class="flex right-64 space-x-4 text-xl font-light float-right absolute">
+                                <li><a href="{{route('dashboard')}}" class="hover:text-gray-300">Dashboard</a></li>
+                                <li><a href="{{route('logout')}}" class="hover:text-gray-300">Logout</a></li>
+                            </ul>
+                        @endif
+                    </div>
+                </nav>
+                
 
-<!-- Navigatiebalk -->
-<nav class="bg-black p-4 text-white">
-    <div class="container mx-auto flex items-center w-full">
-        <a href="{{route('home')}}" class="text-4xl font-bold">Barroc Intens</a>
-        <ul class="flex ml-24 space-x-4 text-xl font-light">
-            <li><a href="{{route('home')}}" class="hover:text-gray-300">Home</a></li>
-            <li><a href="{{route('products.index')}}" class="hover:text-gray-300">Producten</a></li>
-            <li><a href="#contact" class="hover:text-gray-300">Contact</a></li>
-        </ul>
-        @if(\Illuminate\Support\Facades\Auth::user() != null && !\Illuminate\Support\Facades\Auth::user()->hasRole('Customer'))
-            <ul class="flex right-64 space-x-4 text-xl font-light float-right absolute">
-                <li><a href="{{route('dashboard')}}" class="hover:text-gray-300">Dashboard</a></li>
-                <li><a href="{{route('logout')}}" class="hover:text-gray-300">Logout</a></li>
-            </ul>
-        @endif
+            <!-- Diensten-sectie -->
+            <section id="services" class="py-16 relative h-screen">
+                @yield('content')
+            </section>
+            <!-- Footer -->
+            <footer class="bg-black text-white font-light text-center py-4">
+                <p>&copy; 2023 KoffieVerhuur. Alle rechten voorbehouden.</p>
+            </footer>
+        </div>
     </div>
-</nav>
-
-<!-- Diensten-sectie -->
-<section id="services" class="py-16 relative h-screen">
-    @yield('content')
-</section>
-<!-- Footer -->
-<footer class="bg-gray-800 text-white text-center py-4">
-    <p>&copy; 2023 KoffieVerhuur. Alle rechten voorbehouden.</p>
-</footer>
 </body>
 </html>
 
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.guest')
 @section('content')
-    <div class="container mx-auto">
-        <div class="text-center">
-            <h2 class="text-3xl font-bold mb-8">Onze Producten</h2>
-        </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        @foreach($products as $product)
-            <div class="p-6 bg-white rounded-lg shadow-md">
-                @isset($product->image_path)
-                    <img src="{{ asset('storage/' . $product->image_path) }}" class="w-50 rounded-md drop-shadow-lg" alt="{{ $product->name }} afbeelding">
-                @else
-                    <img src="{{ asset('images/no_image_.png') }}" class="w-50 rounded-md drop-shadow-lg" alt="Placeholder afbeelding">
-                @endisset
-                <h3 class="text-xl font-bold mb-2 mt-4">{{$product->name}}</h3>
-                <p>{{$product->description}}</p>
-                <a href="{{route('quote.show', 1)}}" class="mt-4 inline-block border-2 border-yellow-400 hover:bg-yellow-400 duration-200 text-black font-bold py-2 px-6 rounded-full">Offerte</a>
+        <div class="w-10/12 mx-auto border border-1 border-white rounded-lg p-6">
+            <div class="text-center">
+                <h2 class="text-3xl font-bold mb-8 text-white">Onze Producten</h2>
             </div>
-        @endforeach
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                @foreach($products as $product)
+                    <div class="p-6 bg-white rounded-lg shadow-md">
+                        @isset($product->image_path)
+                            <img src="{{ asset('storage/' . $product->image_path) }}" class="w-54 rounded-md drop-shadow-lg" alt="{{ $product->name }} afbeelding">
+                        @else
+                            <img src="{{ asset('images/no_image_.png') }}" class="w-54 rounded-md drop-shadow-lg" alt="Placeholder afbeelding">
+                        @endisset
+                        <h3 class="text-xl font-bold mb-2 mt-4">{{ $product->name }}</h3>
+                        <p>{{ $product->description }}</p>
+                        <a href="{{ route('quote.show', 1) }}" class="mt-4 inline-block border-2 border-yellow-400 hover:bg-yellow-400 duration-200 text-black font-bold py-2 px-6 rounded-full">Offerte</a>
+                    </div>
+                @endforeach
+            </div>
         </div>
-    </div>
 @endsection
