70c4cf6a9e3a93686c9ceb36968df8d2cfcc2210
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2023-12-19 09:49:40 +0100

    kalender fix

--- resources/views/maintenance/fullcalendar.blade.php
+++ resources/views/maintenance/fullcalendar.blade.php
@@ -4,14 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FullCalendar in HTML</title>
-    <link rel="stylesheet" href="https://unpkg.com/@fullcalendar/core/main.css" integrity="sha384-rHmbdTRVo7LK9GSLFnBiI4VIefS3k6ymy2wPnT17LXOBWxh/Xrj4oefDYTkQ9Zjr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/@fullcalendar/daygrid/main.css" integrity="sha384-K8I1IpFgZ5mrTS3WQvmj/Bp7k8V3DNNOzST2/4CQCZQVt/Gr1UxyTQsEfjNU2A6w" crossorigin="anonymous">
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
 </head>
 <body>
     <div id="calendar"></div>
-
-    <script src="https://unpkg.com/@fullcalendar/core/main.min.js" integrity="sha384-4Gr52RTEbcjowYvHRjLlFy2QIzH4JS7uVUQudHTDAZfPeBwlSZ/9sz+AlCeeQQsx" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/@fullcalendar/daygrid/main.min.js" integrity="sha384-tfwwd7rlxVFO6fBQe8GzL4ivR0sDzNKYE/pWpE6GLp9YRRBAIj5KAR5+MGnVeDpN" crossorigin="anonymous"></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
