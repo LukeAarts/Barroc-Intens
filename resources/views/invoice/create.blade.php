f6b0f5847d38d1ef3bcdc026c1560976beff9605
LukeAarts <d280114@edu.rocwb.nl>
2024-01-09 09:07:47 +0100

    Naam van product op factuur tonen
    product_id opgehaald uit database en getoont op factuur

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -67,7 +67,7 @@
                                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="product">
                                         Product naam
                                     </label>
-                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" required name="prodname" id="prodname" type="text" placeholder="Product naam">
+                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" required name="prodname" id="prodname" type="text" placeholder="Product" value="{{$quote->products_id}}">
                                 </div>
                                 <div class="flex flex-wrap -mx-3 mb-6">
                                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="cost">
