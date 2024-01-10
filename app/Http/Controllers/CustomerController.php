9ecefa225c9d9b4662bacbe850ed3cda25655d92
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2024-01-10 22:26:49 +0100

    Email voor klantregistratie (is nog nit af)

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,27 +2,45 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\CustomerRegistration;
 use App\Models\User;
+use Illuminate\Support\Str;
+
 
 class CustomerController extends Controller
 {
-    public function register()
+
+    public function create()
     {
-        // Logica om een klant aan te maken in de database
+        return view('customers.create');
+    }
 
-        // $customer = User::create([
-        //     'name' => request('name'),
-        //     'email' => request('email'),
-        //     'role' => request('role')
-        //     // andere velden die je nodig hebt
-        // ]);
+    public function store(Request $request)
+    {
+        // Valideer de invoer
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            // Voeg andere velden toe die je nodig hebt
+        ]);
+    
+        // Maak een nieuwe klant
+        $customer = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'role' => 'Customer',
+        ]);
 
+        $token = Str::random(40); 
+    
         // Verstuur registratie-e-mail
-        Mail::to($customer->email)->send(new CustomerRegistration($customer));
+        $resetUrl = route('password.reset', ['token' => $token]);
+        Mail::to($customer->email)->send(new CustomerRegistration($customer, $resetUrl, $token));
 
+    
         return "Klant geregistreerd en e-mail verstuurd.";
     }
 }
