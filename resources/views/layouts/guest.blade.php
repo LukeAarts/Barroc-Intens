a96992baa9f504183a9991334337403442decbdc
LukeAarts <d280114@edu.rocwb.nl>
2024-01-30 11:56:56 +0100

    Updated logo on guest page
    Zelfde grootte gegeven, eerst was hij kleiner en niet hetzelfde als de andere navbar.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigatiebalk -->
                 <nav class="p-4 text-white">
                     <div class="container mx-auto flex items-center w-full">
-                        <img src="images/logo6_groot.png" style="width: 65px" alt="afbeelding">
+                        <img src="images/logo6_groot.png" style="width: 95px" alt="afbeelding">
                         <ul class="flex ml-24 space-x-4 text-xl font-light">
                             <li><a href="{{route('home')}}" class="hover:text-gray-300">Home</a></li>
                             <li><a href="{{route('products.index')}}" class="hover:text-gray-300">Producten</a></li>
